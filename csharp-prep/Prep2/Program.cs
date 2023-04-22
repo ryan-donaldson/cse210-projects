c3a4a1b7e06cca6b5aee6124fd9f38d192788ef5
ryan-donaldson <trouvere00@byui.edu>
2023-04-22 14:09:32 -0600

    Completed Preps 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int grade = int.Parse(percentage);
+        string letter = "";
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";;
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You'll have to try again. I know you can do it!");
+        }
+        
+        Console.WriteLine($"Your grade in the class is: {letter}");
     }
 }
