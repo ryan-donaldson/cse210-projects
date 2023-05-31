793e0ebeda68b495ea4e8a75fdf480430308994c
ryan-donaldson <trouvere00@byui.edu>
2023-05-30 22:12:28 -0600

    Learning Activity for week 7

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,20 @@
 using System;
-
-class Program
+namespace Learning04
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning04 World!");
+        static void Main(string[] args)
+        {
+            Assignment assignment1 = new Assignment("Samuel Bennet", "Multiplication");
+            Console.WriteLine(assignment1.GetSummary());
+
+            MathAssignment mathAssignment1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+            Console.WriteLine(mathAssignment1.GetSummary());
+            Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+            WritingAssignment writingAssignment1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+            Console.WriteLine(writingAssignment1.GetSummary());
+            Console.WriteLine(writingAssignment1.GetWritingInformation());
+        }
     }
 }
